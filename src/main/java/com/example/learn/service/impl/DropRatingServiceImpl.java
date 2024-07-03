5def52efd828d719fbfa9cc144e50fa63550e8b0
Vera <inginer719@gmail.ru>
2024-07-03 11:00:47 -0400

    is working

--- src/main/java/com/example/learn/service/impl/DropRatingServiceImpl.java
+++ src/main/java/com/example/learn/service/impl/DropRatingServiceImpl.java
@@ -1,12 +1,17 @@
 package com.example.learn.service.impl;
 
+import com.example.learn.entity.TopUser;
 import com.example.learn.entity.User;
 import com.example.learn.repository.RatingRepository;
+import com.example.learn.repository.TopUserRepository;
+import com.example.learn.service.TopUserService;
 import com.example.learn.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -15,18 +20,25 @@ public class DropRatingServiceImpl {
 
     private final RatingRepository ratingRepository;
     private final UserService userService;
+    private final TopUserService topUserService;
+    private final TopUserRepository topUserRepository;
 
 
     @Scheduled(cron = "0 0 */24 * * *")
     public void deleteRatingUsers(){
 
         // Метод для удаления всех пользователей кроме первых 10
-        public void keepFirstTenRatingUsers() {
 
-            int ratingUsers = 0;
-            if (ratingUsers > 10) {
-                ratingUsers.subList(10, ratingUsers.size()).clear();
-            }
+    }
+
+    //TODO: исправить логику
+    public void keepFirstTenRatingUsers() {
+
+        List<TopUser> topUsers = topUserService.getAllSortDesc();
+        if (topUsers.size() > 10) {
+            List<TopUser> usersToDelete = topUsers.subList(10, topUsers.size());
+            ratingUsers.subList(10, ratingUsers.size()).clear();
         }
     }
 }
+
